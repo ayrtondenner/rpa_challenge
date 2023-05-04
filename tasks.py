07fec41b62994d68143710909b4f980836dc3b18
Ayrton Denner <ayrtondenner_2013@hotmail.com>
2023-05-04 14:17:48 -0300

    Writer update

--- tasks.py
+++ tasks.py
@@ -1,7 +1,9 @@
 from helpers import variables_helper
 from helpers.debug_helper import print_debug_log
 
+from writers.XlsxWriter import XlsxWriter
 from writers.CSVWriter import CSVWriter
+
 from crawlers.NYTCrawler import NYTCrawler
 
 # Define a main() function that calls the other functions in order:
@@ -14,7 +16,8 @@ def main():
         crawler = NYTCrawler()
         article_info_list = crawler.extract_data(variables)
 
-        report_writer = CSVWriter(article_info_list)
+        #report_writer = CSVWriter(article_info_list)
+        report_writer = XlsxWriter(article_info_list)
         report_writer.write_report()
     except Exception as ex:
         print_debug_log(f"Error: {ex}")
